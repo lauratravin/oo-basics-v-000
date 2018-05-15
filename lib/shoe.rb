6bfbbede7189d623cde85401df449e95c58ba8fe
Laura Messa Travin <laura.travin@gmail.com>
2018-05-15 17:20:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  def initialize(b)
+    @title = b
+  end
+  def brand
+   @brand
+  end
+
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
+end
